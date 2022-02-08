6dee5ec9a64afdd55c0ec1760f2e866f95438c42
Htun Aung Lin <mr.htunaunglin@gmail.com>
2022-02-08 19:56:51 +0630

    app.js setup for main js endpoint

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 require('./bootstrap');
+
+import { createApp } from 'vue';
+import Notifications from 'notiwind';
+import App from './components/App.vue';
+import router from './router';
+import store from './store';
+
+axios.defaults.withCredentials = true;
+
+store.dispatch('getUser').then(() => {
+    createApp(App)
+        .use(router)
+        .use(store)
+        .use(Notifications)
+        .mount('#app');
+});
