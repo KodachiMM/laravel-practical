6c294c7e23dc337304c419bec10f890398744283
Htun Aung Lin <mr.htunaunglin@gmail.com>
2022-02-08 19:49:59 +0630

    form submit event listener feature

---
+++ app/Events/FormSubmitted.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Form;
+use App\Models\User;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class FormSubmitted
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(public User $user, public Form $form)
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('form-submitted');
+    }
+}
---
+++ app/Listeners/SendFormSubmitNotification.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\FormSubmitted;
+use App\Mail\FormSubmitted as MailFormSubmitted;
+use Illuminate\Support\Facades\Mail;
+
+class SendFormSubmitNotification
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\FormSubmitted  $event
+     * @return void
+     */
+    public function handle(FormSubmitted $event)
+    {
+        try {
+            Mail::to($event->user)->send(new MailFormSubmitted($event->form));
+        } catch (\Exception$e) {
+            logger()->critical($e);
+        }
+    }
+}
