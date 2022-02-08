a2e8a145147b3dd82daaef9349a091a3fd0b707a
Htun Aung Lin <mr.htunaunglin@gmail.com>
2022-02-08 19:55:22 +0630

    add app.blade.php for initial view

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <title>Laravel Practical</title>
+</head>
+<body class="bg-gray-100 max-h-screen antialiased leading-none ">
+    <div id="app"></div>
+
+    <script src="{{ mix('/js/manifest.js') }}" defer></script>
+    <script src="{{ mix('/js/vendor.js') }}" defer></script>
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+</body>
+</html>
